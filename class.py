ecd7f61e38a8b308c78c8b9b24ed24720599825d
Eyasin Arafath <175514956+Eyasin707@users.noreply.github.com>
2024-07-14 10:14:36 +0600

    class.py

--- class.py
+++ class.py
@@ -8,3 +8,18 @@ class person():
         return f"The name of this person is: {self.name} and his age is: {self.age}"
 person1=person("Jhon", 23)
 print(person1)
+
+
+
+#example:
+
+class person:
+    def __init__(self, name, age, loc):
+        self.name= name
+        self.age = age
+        self.loc= loc
+    def abc(self):
+        print(f"Hello! My name is {self.name}, I'm only {self.age} years old and I live at {self.loc}")
+p1= person("Jhon", 23, "NYC")
+p1.abc()
+
