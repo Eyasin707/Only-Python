e99b2ef5137b1914f1101316a94ac536620a9ff7
Eyasin Arafath <175514956+Eyasin707@users.noreply.github.com>
2024-07-15 15:22:47 +0600

    Update Class.py

--- Class.py
+++ Class.py
@@ -39,85 +39,103 @@ p3= my_class_4(name, age)
 #Now we mix all it up and write our final code.
 #this time we will try to make it complex.
 
-class my_class_5:
-    def __init__(self,name,age,stu_id,mark_e,mark_h,mark_s,mark_m,mark_art):
+class my_class:
+    def __init__(self,name,age,stu_id,m_e,m_h,m_s,m_m,m_art):
         self.name= name
         self.age= age
         self.stu_id= stu_id
-        self.mark_e= mark_e
-        self.mark_h= mark_h
-        self.mark_s= mark_s
-        self.mark_m= mark_m
-        self.mark_art= mark_art
+        self.m_e= m_e
+        self.m_h= m_h
+        self.m_s= m_s
+        self.m_m= m_m
+        self.m_art= m_art
         
-        avg= (mark_e+mark_h+mark_s+mark_m+mark_art)/5
-        print(f'This is {self.name} and his ID is {self.stu_id}.'
-             f'He has obtained these folliwing marks:\n'
-             f'English: {self.mark_e}\n'
-             f'History: {self.mark_h}\n'
-             f'Science: {self.mark_s}\n'
-             f'Math: {self.mark_m}\n'
-             f'Art: {self.mark_art}\n'
-             f'His average mark in this semester is: {avg}\n'
-             f'He\'s grades are:\n'
-             f'English: {self.grade(self.mark_e)}\n'
-             f'History: {self.grade(self.mark_h)}\n'
-             f'Science: {self.grade(self.mark_s)}\n'
-             f'Math: {self.grade(self.mark_m)}\n'
-             f'Art: {self.grade(self.mark_art)}')
+        avg= (m_e+m_h+m_s+m_m+m_art)/5
+        
+        self.grade_e= self.grade(self.m_e)
+        self.grade_h= self.grade(self.m_h)
+        self.grade_s= self.grade(self.m_s)
+        self.grade_m= self.grade(self.m_m)
+        self.grade_art= self.grade(self.m_art)
+        
+        
+        print(f'This is {self.name}, ID: {self.stu_id} and he is\n'
+              f'{self.age} years old. He\'s marks in this semester\n'
+              f'are:\n'
+              f'English= {self.m_e}\n'
+              f'History= {self.m_h}\n'
+              f'Science= {self.m_s}\n'
+              f'Math= {self.m_m}\n'
+              f'Art= {self.m_art}\n'
+              f'Hi\'s average maark is {avg}\n'
+              f'He\'s grade distribution:'
+              f'English: {self.grade_e}\n'
+              f'History: {self.grade_h}\n'
+              f'Science: {self.grade_s}\n'
+              f'Math: {self.grade_m}\n'
+              f'Art: {self.grade_art}')
+        
         
     def grade(self,marks):
         if marks>90:
-            print("A+")
+            return "A+"
         elif marks>80:
-            print("A")
+            return "A"
         elif marks>70:
-            print("A-")
+            return "B"
         elif marks>60:
-            print("B")
-        elif marks>50:
-            print("C")
+            return "C"
         else:
-            print("F")
+            return "F"
             
-name= input("Enter name:")
-age= input("Enter age")
-stu_id= int(input("Enter Student ID:"))
-mark_e= int(input("Marks in English:"))   
-mark_h= int(input("Marks in History:"))
-mark_s= int(input("Marks in Science:")) 
-mark_m= int(input("Marks in Math:")) 
-mark_art= int(input("Marks in Art:")) 
+name = input("Enter Name: ").strip()  # Ensure no extra whitespace affects input
+while True:
+    try:
+        age = int(input("Enter Age: "))
+        break  # Exit the loop if conversion to int is successful
+    except ValueError:
+        print("Please enter a valid integer for Age.")
 
-student= my_class_5(name,age,stu_id,mark_e,mark_h,mark_s,mark_m,mark_art)
+while True:
+    try:
+        stu_id = int(input("Enter student ID: "))
+        break  # Exit the loop if conversion to int is successful
+    except ValueError:
+        print("Please enter a valid integer for Student ID.")
 
+while True:
+    try:
+        m_e = int(input("Enter English Marks: "))
+        break  # Exit the loop if conversion to int is successful
+    except ValueError:
+        print("Please enter a valid integer for English Marks.")
 
+while True:
+    try:
+        m_h = int(input("Enter History Marks: "))
+        break  # Exit the loop if conversion to int is successful
+    except ValueError:
+        print("Please enter a valid integer for History Marks.")
 
-'''
-Enter name: rr
-Enter age 5
-Enter Student ID: 5
-Marks in English: 5
-Marks in History: 5
-Marks in Science: 5
-Marks in Math: 5
-Marks in Art: 5
-F
-F
-F
-F
-F
-This is rr and his ID is 5.He has obtained these folliwing marks:
-English: 5
-History: 5
-Science: 5
-Math: 5
-Art: 5
-His average mark in this semester is: 5.0
-He's grades are:
-English: None
-History: None
-Science: None
-Math: None
-Art: None
-'''
+while True:
+    try:
+        m_s = int(input("Enter Science Marks: "))
+        break  # Exit the loop if conversion to int is successful
+    except ValueError:
+        print("Please enter a valid integer for Science Marks.")
+
+while True:
+    try:
+        m_m = int(input("Enter Math Marks: "))
+        break  # Exit the loop if conversion to int is successful
+    except ValueError:
+        print("Please enter a valid integer for Math Marks.")
+
+while True:
+    try:
+        m_art = int(input("Enter Art Marks: "))
+        break  # Exit the loop if conversion to int is successful
+    except ValueError:
+        print("Please enter a valid integer for Art Marks.")
+
+student= my_class(name,age,stu_id,m_e,m_h,m_s,m_m,m_art)
